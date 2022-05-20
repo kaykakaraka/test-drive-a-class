a2504b3cf3ea00e8a6fe058c3e8b7820f2d67cec
Kay <kard@hotmail.co.uk>
2022-05-20 14:00:05 +0100

    Completed diary entry program and tests

---
+++ lib/diary_entry.rb
@@ -0,0 +1,34 @@
+class DiaryEntry
+  def initialize(title, contents) # title, contents are strings
+    @title = title
+    @contents = contents
+    @begin_from = 0
+  end
+
+  def title
+    @title
+  end
+
+  def contents
+    @contents
+  end
+
+  def count_words
+    @contents.split(" ").count
+  end
+
+  def reading_time(wpm) # wpm is an integer representing the number of words the
+    (count_words.to_f / wpm).round                   # user can read per minute
+  end
+
+  def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
+    @begin_from = 0 if @begin_from >= count_words - 1 #begin from the beginning if end of text has been reached
+
+    end_at = @begin_from + (wpm * minutes - 1) #calculates where to end reading
+    chunk_to_read = @contents.split(" ")[@begin_from..(end_at)].join(" ")   #extracts the chunk of text 
+                          
+    @begin_from += chunk_to_read.split(" ").count
+    
+    chunk_to_read                
+  end
+end
---
+++ spec/diary_entry_spec.rb
@@ -0,0 +1,61 @@
+require "diary_entry"
+
+RSpec.describe DiaryEntry do
+  it "returns the title as a string" do
+    diary_entry = DiaryEntry.new("Tuesday morning", "We went for a lovely walk.")
+    result = diary_entry.title
+    expect(result).to eq "Tuesday morning"
+  end
+
+  it "returns the contents as a string" do
+    diary_entry = DiaryEntry.new("Tuesday morning", "We went for a lovely walk.")
+    result = diary_entry.contents
+    expect(result).to eq "We went for a lovely walk."
+  end
+
+  it "returns the number of words in the contents of the diary as an integer" do
+    diary_entry = DiaryEntry.new("The third of June", "What a wonderful day! I had a great time.") 
+    result = diary_entry.count_words
+    expect(result).to eq 9
+  end
+
+  it "returns an estimate of reading time for the user for the contents of the diary" do
+    diary_entry = DiaryEntry.new("The fourth of June", "content " * 400)
+    result = diary_entry.reading_time(100)
+    expect(result).to eq 4
+  end
+
+  it "returns a rounded estimate of reading time" do
+    diary_entry = DiaryEntry.new("The fifth of June", "content " * 388)
+    result = diary_entry.reading_time(50)
+    expect(result).to eq 8
+  end
+
+  it "returns a chunk of text that the reader could read in a given time" do
+    diary_entry = DiaryEntry.new("The sixth of June", "content " * 8000)
+    result = diary_entry.reading_chunk(1, 4)
+    expect(result).to eq ("content content content content")
+  end
+
+  it "returns the next chunk of text that the reader could read in a given time when asked twice" do
+    diary_entry = DiaryEntry.new("The sixth of June", "I absolutely love reading. It is such a pleasure!!")
+    first_read = diary_entry.reading_chunk(1, 4)
+    result = diary_entry.reading_chunk(1,5)
+    expect(result).to eq ("It is such a pleasure!!")
+  end
+
+  it "returns to the beginning of the text if the reader has read to the end" do
+    diary_entry = DiaryEntry.new("The sixth of June", "I absolutely love reading. It is such a pleasure!!")
+    first_read = diary_entry.reading_chunk(1, 4)
+    second_read = diary_entry.reading_chunk(1, 5)
+    result = diary_entry.reading_chunk(1, 3)
+    expect(result).to eq "I absolutely love"
+  end
+
+  it "stops at the end of the text even if reader has more time to read" do
+    diary_entry = DiaryEntry.new("The seventh of June", "I absolutely love reading. It is such a pleasure!!")
+    first_read = diary_entry.reading_chunk(1, 4)
+    result = diary_entry.reading_chunk(20, 5)
+    expect(result).to eq "It is such a pleasure!!"
+  end
+end
